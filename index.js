d4cf98aac0982de4b06f46a73f4b2e0b8c311080
LightspeedCoder <48801499+LightspeedCoder@users.noreply.github.com>
2019-05-10 15:11:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,3 +20,15 @@ function preload() {
 function update() {
 
 }
+
+var platforms;
+
+function create ()
+{
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+}
