e7c79418229e01506b182ba186a87e226600d29f
LightspeedCoder <48801499+LightspeedCoder@users.noreply.github.com>
2019-05-14 14:24:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var game = new Phaser.Game(config);
 function create (){
   player.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
   player.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+  player.cursors = this.input.keyboard.createCursorKeys();
   player.main = this.add.sprite(100,100,'player');
   player.main.setCollideWorldBounds(true)
 }
@@ -28,16 +29,7 @@ function update() {
     player.main.x += 5
   } else if (player.cursors.left.isDown || player.keyA.isDown) {
     player.main.x -= 5
-  } else if (player.cursors.up.isDown || player.keyW.isDown) {
-    player.main.y -= 5
-  } if(player.keyW.isDown && player.keyA.isDown || player.cursors.up.isDown && player.cursors.left.isDown) {
-    player.main.y -= 5;
-    player.main.x -= 5;
-  } if(player.keyW.isDown && player.keyD.isDown || player.cursors.up.isDown && player.cursors.right.isDown) {
-    player.main.y -= 5;
-    player.main.x += 5;
   }
-}
 
 var platforms;
 
