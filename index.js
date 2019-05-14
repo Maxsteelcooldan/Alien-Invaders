73e0a55a6313da9be6ab61797fe710f4f5703707
LightspeedCoder <48801499+LightspeedCoder@users.noreply.github.com>
2019-05-14 14:10:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ var game = new Phaser.Game(config);
 function create (){
   player.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
   player.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-  player.main.setCollideWorldBounds(true)
   player.main = this.add.sprite(100,100,'player');
+  player.main.setCollideWorldBounds(true)
 }
 function preload() {
   this.load.image('player', 'alien.png')
